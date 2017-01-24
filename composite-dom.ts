d27b0222fa3369adf1ee34f0e6f46f61b8bddc22
Katrina Saulon <ksaulon777@gmail.com>
2017-01-24 11:10:09 -0800

    Add files via upload

--- composite-dom.ts
+++ composite-dom.ts
@@ -2,3 +2,24 @@ interface IDomElement {
   print();
 }
 
+class DomElement {
+  print():string {"<html> // DomElement"};
+}
+
+class Dom {
+  public name:'name';
+  add(element: IDomElement) {
+  
+  }
+}
+
+class TextNode {
+  print;
+}
+
+HTMLElement.print();
+
+
+
+
+
