49e408ef24841d36ae6371b86da18427031b3258
Katrina Saulon <ksaulon777@gmail.com>
2017-01-24 11:08:13 -0800

    Update composite-dom.ts

--- composite-dom.ts
+++ composite-dom.ts
@@ -2,3 +2,19 @@ interface IDomElement {
   print();
 }
 
+class DomElement {
+  print():string {"<html> // DomElement"};
+}
+
+class Dom {
+  public name:'name';
+  add(element: IDomElement) {
+  
+  }
+}
+
+class TextNode {
+  print;
+}
+
+HTMLElement.print();
